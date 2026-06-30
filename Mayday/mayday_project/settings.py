395b5c48d0ce471131442eeb889e94c602ca13ba
taobb123 <134066860+taobb123@users.noreply.github.com>
2026-06-30 17:30:01 +0800

    播放器不受页面操作的打断

--- Mayday/mayday_project/settings.py
+++ Mayday/mayday_project/settings.py
@@ -105,7 +105,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Music directory path
-MUSIC_DIRECTORY = r'D:\Music\梁静茹'
+MUSIC_DIRECTORY = r'D:\Music\郑秀文'
 
 # Lyrics directory path
 LYRICS_DIRECTORY = r'C:\Lyrics'
